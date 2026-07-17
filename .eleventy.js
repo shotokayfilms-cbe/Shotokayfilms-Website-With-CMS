5405358f98e2d54c15b1193916af985b3acd6bb4
shot okay films <shotokayfilms@gmail.com>
2026-07-17 22:05:10 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,18 +5,6 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
 
-  // Automatically collect every Markdown file inside /projects
-  eleventyConfig.addCollection("portfolioProjects", function (collectionApi) {
-    return collectionApi
-      .getFilteredByGlob("./projects/*.md")
-      .sort((a, b) => {
-        const orderA = Number(a.data.order || 0);
-        const orderB = Number(b.data.order || 0);
-
-        return orderB - orderA;
-      });
-  });
-
   return {
     dir: {
       input: ".",
