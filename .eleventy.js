60c63e973d45a8897dd33098513b7387ce39ff1e
shotokayfilms-cbe <shotokayfilms@gmail.com>
2026-07-17 18:46:38 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,21 @@
 export default function (eleventyConfig) {
+  // Copy static folders directly to the built website
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
+
+  // Automatically collect every Markdown file inside /projects
+  eleventyConfig.addCollection("projects", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("./projects/*.md")
+      .sort((a, b) => {
+        const orderA = Number(a.data.order || 0);
+        const orderB = Number(b.data.order || 0);
+
+        return orderB - orderA;
+      });
+  });
 
   return {
     dir: {
